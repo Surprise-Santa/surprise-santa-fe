cfce71c0ac5717be81b51c3660013d23f283a89c
Enyia Christian <94331754+Christiano112@users.noreply.github.com>
2023-11-25 22:20:08 +0100

    created faq and footer sections

---
+++ src/lib/dummyData.ts
@@ -0,0 +1,18 @@
+export const faqData = [
+    {
+        question: "What is Secret Santa?",
+        answer: "Secret Santa is a Christmas tradition. It involves a group of people exchanging gifts with each other, but not knowing who is giving them a gift. This is done by randomly assigning each person in the group another person to give a gift to. This is done anonymously, so the person receiving the gift doesn't know who it is from. The person giving the gift doesn't know who they are giving a gift to either. This is why it is called Secret Santa.",
+    },
+    {
+        question: "How do i create an event?",
+        answer: "You can create an event by clicking on the create event button on your dashboard. You can then fill in the details of the event and submit.",
+    },
+    {
+        question: "How many people can participate?",
+        answer: "You can have as many people as you want in your event. However, we recommend that you have at least 3 people in your event.",
+    },
+    {
+        question: "How can I invite people to my event?",
+        answer: "You can invite people to your event by sharing the event link with them. You can find the event link on the event page.",
+    },
+];
