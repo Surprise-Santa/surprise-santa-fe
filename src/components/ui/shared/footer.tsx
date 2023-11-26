79ea3ccbdef23cd68154adb4144f162ad476a7af
meldy2503 <emelder.charles25@gmail.com>
2023-11-26 23:35:39 +0100

    feat: add landing page and navbar components

--- src/components/ui/shared/footer.tsx
+++ src/components/ui/shared/footer.tsx
@@ -3,33 +3,35 @@ import { FacebookIcon, TwitterIcon, LinkedinIcon } from "lucide-react";
 
 const Footer = () => {
     return (
-        <footer className="bg-primary-green p-8 flex flex-col sm:flex-row gap-8 sm:gap-12 items-center justify-between">
-            <p className="text-white font-bold text-2xl">Stay in touch with us</p>
+        <footer className="bg-primary-green ">
+            <div className="max-w-[1280px] mx-auto py-8 flex flex-col sm:flex-row gap-8 sm:gap-12 items-center justify-between">
+            <p className="text-white font-semiBold text-2xl">Stay in touch with us</p>
             <div className="flex items-center gap-6">
                 <Link
                     href="#"
                     target="_blank"
                     rel="noreferrer noopener"
-                    className="w-8 h-8 rounded-full bg-white grid place-items-center hover:scale-105 hover:text-primary-green"
+                    className="w-10 h-10 rounded-full bg-white grid place-items-center hover:scale-105 hover:text-primary-green"
                 >
-                    <FacebookIcon size={16} />
+                    <FacebookIcon size={16} fill="black" color="black"/>
                 </Link>
                 <Link
                     href="#"
                     target="_blank"
                     rel="noreferrer noopener"
-                    className="w-8 h-8 rounded-full bg-white grid place-items-center hover:scale-105 hover:text-primary-green"
+                    className="w-10 h-10 rounded-full bg-white grid place-items-center hover:scale-105 hover:text-primary-green"
                 >
-                    <TwitterIcon size={16} />
+                    <TwitterIcon size={16} fill="black" color="black"/>
                 </Link>
                 <Link
                     href="#"
                     target="_blank"
                     rel="noreferrer noopener"
-                    className="w-8 h-8 rounded-full bg-white grid place-items-center hover:scale-105 hover:text-primary-green"
+                    className="w-10 h-10 rounded-full bg-white grid place-items-center hover:scale-105 hover:text-primary-green"
                 >
-                    <LinkedinIcon size={16} />
+                    <LinkedinIcon size={16} fill="black" color="black"/>
                 </Link>
+            </div>
             </div>
         </footer>
     );
