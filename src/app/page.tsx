452f6ebc770959567eec22c15e3ff8fb11d28f79
Taladeogo Abraham <ataladeogo@gmail.com>
2023-11-27 11:09:41 +0100

    Add "How It Works" section to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import SectionThree from "../components/ui/home-page-sections/section-3";
 import Footer from "@/components/ui/shared/footer";
 import LandingPage from "@/components/ui/home-page-sections/landing-page";
 import Navbar from "@/components/ui/shared/navbar";
+import HowItWorks from "@/components/ui/home-page-sections/how-it-works";
 
 export default function Home() {
     return (
         <>
             <Navbar />
             <LandingPage />
+            <HowItWorks />
             <SectionThree />
             <Faq />
             <Footer />
---
+++ src/components/ui/home-page-sections/how-it-works.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+import HowItWorksCard from "@/components/how-it-works-text-card/how-it-works-card";
+import { howItWorksData } from "@/lib/dummyData";
+
+const HowItWorks = () => {
+    return (
+        <section className="w-full py-8 md:py-20 px-5 xl:px-[136px]">
+            <h2 className="text-center font-bold text-[31px] md:mb-10">How Does It Work?</h2>
+
+            <div className="flex flex-col gap-6 md:gap-0">
+                {howItWorksData.map((card) => (
+                    <div
+                        key={card.index}
+                        className="flex flex-col md:flex-row items-center w-full md:gap-24 gap-4 md:h-96 h-full"
+                    >
+                        {card.index % 2 === 0 ? (
+                            <>
+                                <HowItWorksCard
+                                    title={card.title}
+                                    description={card.description}
+                                    backgroundImage={card.backgroundImage}
+                                />
+                                <div className="w-1/2">
+                                    <Image
+                                        src={card.image}
+                                        alt={`Illustration for ${card.title}`}
+                                        width={300}
+                                        height={300}
+                                        className="w-full h-full hidden md:block"
+                                    />
+                                </div>
+                            </>
+                        ) : (
+                            <>
+                                <div className="w-1/2">
+                                    <Image
+                                        src={card.image}
+                                        alt={`Illustration for ${card.title}`}
+                                        width={300}
+                                        height={300}
+                                        className="w-full h-full hidden md:block"
+                                    />
+                                </div>
+                                <HowItWorksCard
+                                    title={card.title}
+                                    description={card.description}
+                                    backgroundImage={card.backgroundImage}
+                                />
+                            </>
+                        )}
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
+};
+
+export default HowItWorks;
