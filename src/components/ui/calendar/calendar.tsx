a36011b46e36911cf6c158bbb0b73c23e1c2158c
Enyia Christian <94331754+Christiano112@users.noreply.github.com>
2023-12-08 05:21:18 +0100

    fixes

--- src/components/ui/calendar/calendar.tsx
+++ src/components/ui/calendar/calendar.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { useState, useCallback } from 'react';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import { useState, useCallback } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 import { isSameDay } from "date-fns";
-import ClientWrapper from '@/components/wrapper/client';
+import ClientWrapper from "@/components/wrapper/client";
 
 type ValuePiece = Date | null;
 
@@ -19,25 +19,29 @@ const ViewCalendar = () => {
     const [value, onChange] = useState<Value>(new Date());
 
     const tileContent = useCallback(({ date, view }: TileContentProp) => {
-        const datesToAddContentTo = [new Date(("23 Dec 2023")), new Date()];
+        const datesToAddContentTo = [new Date("23 Dec 2023"), new Date()];
 
         // Add class to tiles in month view only
-        if (view === 'month') {
+        if (view === "month") {
             // Check if a date React-Calendar wants to check is on the list of dates to add class to
-            if (datesToAddContentTo.find(dDate => isSameDay(dDate, date))) {
-                return 'Secret Santa';
+            if (datesToAddContentTo.find((dDate) => isSameDay(dDate, date))) {
+                return "Secret Santa";
             }
         }
-    }, [])
+    }, []);
 
     return (
-        <Calendar onChange={onChange} value={value} tileContent={tileContent} tileClassName="gap-2 grid place-items-center" />
-    )
-}
+        <Calendar
+            onChange={onChange}
+            value={value}
+            tileContent={tileContent}
+            tileClassName="gap-2 grid place-items-center"
+        />
+    );
+};
 
 export const AppCalendar = () => (
     <ClientWrapper>
         <ViewCalendar />
     </ClientWrapper>
-)
-
+);
