9b29c95f9bec98c1eafce1cb1f37862e3917267d
Enyia Christian <94331754+Christiano112@users.noreply.github.com>
2023-12-13 21:57:25 +0100

    integrated groups and events api

--- src/services/queries/groups.ts
+++ src/services/queries/groups.ts
@@ -1,10 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "@/services/axios";
 import { urls } from "../urls";
+import { GroupTypes } from "@/types/groups";
 
 export const useGetAllGroups = () => {
     return useQuery(["getAllGroups"], async () => {
         const res = await axios.get(urls.getAllGroupsUrl);
-        return res.data.data;
+        return res.data.data as GroupTypes[];
     });
 };
--- src/types/groups/index.ts
+++ src/types/groups/index.ts
@@ -3,3 +3,45 @@ export interface GroupType {
     name: string;
     description: string;
 }
+
+export interface User {
+    createdAt: string;
+    createdBy: string | null;
+    email: string;
+    firstName: string;
+    gender: string;
+    id: string;
+    lastName: string;
+    middleName: string;
+    phone: string;
+    profileImgUrl: string;
+    status: boolean;
+    updatedAt: string;
+    updatedBy: string | null;
+}
+
+export interface Member {
+    createdAt: string;
+    createdBy: string | null;
+    groupId: string;
+    id: string;
+    updatedAt: string;
+    updatedBy: string | null;
+    user: User;
+    userId: string;
+}
+
+export interface GroupTypes {
+    createdAt: string;
+    createdBy: string;
+    description: string;
+    groupLink: string;
+    id: string;
+    isPublic: boolean;
+    logoUrl: string;
+    members: Member[];
+    name: string;
+    status: boolean;
+    updatedAt: string;
+    updatedBy: string | null;
+}
