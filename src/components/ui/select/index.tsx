bec3d292b690a08448312936944696ada29adcb1
meldy2503 <emelder.charles25@gmail.com>
2023-12-14 03:12:06 +0100

    fix: resolve conflicts

--- src/components/ui/select/index.tsx
+++ src/components/ui/select/index.tsx
@@ -105,9 +105,9 @@ const SelectLabel = React.forwardRef<
 SelectLabel.displayName = SelectPrimitive.Label.displayName;
 
 const SelectItem = React.forwardRef<
-    React.ElementRef<typeof SelectPrimitive.Item> ,
-    React.ComponentPropsWithoutRef<typeof SelectPrimitive.Item> 
- >(({ className, children, ...props }, ref) => (
+    React.ElementRef<typeof SelectPrimitive.Item>,
+    React.ComponentPropsWithoutRef<typeof SelectPrimitive.Item>
+>(({ className, children, ...props }, ref) => (
     <SelectPrimitive.Item
         ref={ref}
         className={cn(
