7c94e8d68964b73a0d5a15684b614a8d10c9d8dd
meldy2503 <emelder.charles25@gmail.com>
2023-12-20 10:47:44 +0100

    feat: pull from main branch

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,33 +1,30 @@
-
-
-
 export type CursorType = { cursor: string; isCurrent: boolean; page: number };
 
 export type PageEdge<T> = {
-  cursor: string;
-  node: T;
+    cursor: string;
+    node: T;
 };
 
 export type PageCursors = {
-  around: CursorType[];
-  next: CursorType;
-  previous: CursorType;
+    around: CursorType[];
+    next: CursorType;
+    previous: CursorType;
 };
 
 export type DataType<T> = {
-  pageCursors: PageCursors;
-  pageEdges: PageEdge<T>[];
-  totalCount: number;
+    pageCursors: PageCursors;
+    pageEdges: PageEdge<T>[];
+    totalCount: number;
 };
 
 export type QueryParamsType = {
-  term?: string;
-  status?: boolean;
-  size?: number;
-  direction?: string;
-  cursor?: string;
-  mode?: string;
-  orderBy?: string;
-  departmentId?: string
-  [key: string]: any;
+    term?: string;
+    status?: boolean;
+    size?: number;
+    direction?: string;
+    cursor?: string;
+    mode?: string;
+    orderBy?: string;
+    departmentId?: string;
+    [key: string]: any;
 };
